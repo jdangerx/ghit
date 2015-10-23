8763f92251d5a7a8046c3637ede314309693d876
John Xia <john.danger.xia@gmail.com>
2015-10-23 15:24:36 -0400

    Need to use version and numEntries in later parsing so pull it out of indexheader

--- Index.hs
+++ Index.hs
@@ -5,25 +5,19 @@ import qualified Data.ByteString as BS
 import qualified Data.Attoparsec.ByteString as A
 
 
-data Index = Index {
-  headerOf :: IndexHeader
+data Index = Index { versionOf :: Int
+                   , numEntriesOf :: Int
   }
            deriving Show
 
-data IndexHeader = IndexHeader { versionOf :: Int
-                               , numEntriesOf :: Int }
-                   deriving Show
-
-parseHeader :: A.Parser IndexHeader
-parseHeader = IndexHeader <$> (A.string "DIRC" *> parse32Bit) <*> parse32Bit
+index :: A.Parser Index
+index = do
+  A.string "DIRC"
+  version <- parse32Bit
+  numEntries <- parse32Bit
+  return $ Index version numEntries
 
 parse32Bit :: A.Parser Int
 parse32Bit = do
-  bit1 <- A.anyWord8
-  bit2 <- A.anyWord8
-  bit3 <- A.anyWord8
-  bit4 <- A.anyWord8
-  return . fromIntegral $ bit4 + bit3 * 2^8 + bit2 * 2^16 + bit1 * 2^24
-
-parseIndex :: A.Parser Index
-parseIndex = Index <$> parseHeader
+  bytes <- A.count 4 A.anyWord8
+  return . foldl (\acc i -> acc * 256 + fromIntegral i) 0 $ bytes
