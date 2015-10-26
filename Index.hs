b8b971a104b0bd5ad8ce24134ccd895f8d4fd34b
John Xia <john.danger.xia@gmail.com>
2015-10-26 11:57:09 -0400

    Adding extension parsing capability for CachedTree.

--- Index.hs
+++ Index.hs
@@ -13,6 +13,8 @@ import qualified Data.Attoparsec.ByteString as A
 data Index = Index { versionOf :: Int
                    , numEntriesOf :: Int
                    , entriesOf :: [Entry]
+                   , extensionsOf :: [Extension]
+                   , checksumOf :: BS.ByteString
   }
            deriving Show
 
@@ -30,8 +32,19 @@ data Entry = Entry { ctimeOf :: Int
   }
            -- deriving Show
 
+data Extension = Link { sharedIndexOf :: BS.ByteString
+                      , deleteBitMapOf :: BS.ByteString
+                      , replaceBitMapOf :: BS.ByteString }
+               | CachedTree { pathOf :: BS.ByteString
+                            , numEntriesInCachedTree :: Int
+                            , numSubtreesOf :: Int
+                            , objNameOf :: BS.ByteString }
+               | Ext { signatureOf :: BS.ByteString
+                     , extContentOf :: BS.ByteString }
+                 deriving Show
+
 instance Show Entry where
-  show (Entry {shaOf = sha, flagsOf = flags, entryPathOf = entryPath}) =
+  show (Entry {shaOf = sha, entryPathOf = entryPath}) =
     -- "SHA: " ++ toHexes sha ++ ", " ++ "flags: " ++ show flags ++ ", " ++ show entryPath
     "SHA: " ++ toHexes sha ++ ", " ++ show entryPath
 
@@ -44,33 +57,13 @@ data ObjType = File | Symlink | Gitlink deriving Show
 type Permission = Int
 
 data Flags = Flags { assumeValidOf :: Bool
-                     , extendedOf :: Bool
-                     , stageOf :: Int
-                     , nameLenOf :: Int
-                     , reservedOf :: Maybe Bool
-                     , skipWorktreeOf :: Maybe Bool
-                     , intentToAddOf :: Maybe Bool
-                     }
+                   , extendedOf :: Bool
+                   , stageOf :: Int
+                   , nameLenOf :: Int
+                   , reservedOf :: Maybe Bool
+                   , skipWorktreeOf :: Maybe Bool
+                   , intentToAddOf :: Maybe Bool }
              deriving Show
-
-makeV2Flags :: Int -> Flags
-makeV2Flags i =
-  let assumeValid = i .&. 2^(15::Int) == 1
-      extended = i .&. 2^(14::Int) == 1
-      stage = i .&. (2^(13::Int) + 2^(12::Int))
-      nameLen = i `mod` 2^12
-  in Flags assumeValid extended stage nameLen Nothing Nothing Nothing
-
-makeV3Flags :: Int -> Int -> Flags
-makeV3Flags first second =
-  let v2 = makeV2Flags first
-      reserved = second .&. 2^(15::Int) == 1
-      skipWorktree = second .&. 2^(14::Int) == 1
-      intentToAdd = second .&. 2^(13::Int) == 1
-  in v2 { reservedOf = Just reserved
-        , skipWorktreeOf = Just skipWorktree
-        , intentToAddOf = Just intentToAdd }
-
 
 index :: A.Parser Index
 index = do
@@ -78,20 +71,10 @@ index = do
   version <- parse32Bit
   numEntries <- parse32Bit
   entries <- A.count numEntries $ entry version
-  return $ Index version numEntries entries
-
-billion :: Int
-billion = 10 ^ (9 :: Int)
-
-getMode :: Int -> Mode
-getMode i =
-  let typeBits = i `div` 2 ^ (12 :: Int)
-      permBits = i `mod` 1024
-      objType = case typeBits of
-                 8 -> File
-                 10 -> Symlink
-                 14 -> Gitlink
-  in Mode objType permBits
+  exts <- A.many' extension
+  -- let exts = []
+  checksum <- A.take 20
+  return $ Index version numEntries entries exts checksum
 
 entry :: Int -> A.Parser Entry
 entry version =
@@ -111,14 +94,65 @@ entry version =
               then makeV2Flags <$> parse16Bit -- 62, or...
               else do [first, second] <- A.count 2 parse16Bit -- 64
                       return $ makeV3Flags first second
-     entryPath <- A.take (nameLenOf flags) -- 62 or 64 + nameLen + 1
-     let numBytesPadding = case version of -- needs to be 1 to 8 bytes
+     entryPath <- A.take (nameLenOf flags) -- 62 or 64 + nameLen + 1 (for NUL termination)
+     -- padding needs to be 1 to 8 bytes so we add 1, unless we are in version 4
+     let numBytesPadding = case version of
                             2 -> (8 - (63 + nameLenOf flags) `mod` 8) `mod` 8 + 1
                             3 -> (8 - (65 + nameLenOf flags) `mod` 8) `mod` 8 + 1
                             4 -> 0
      A.take numBytesPadding
      return $ Entry ctime mtime device ino mode uid gid fileSize sha flags entryPath
 
+cachedTree :: A.Parser Extension
+cachedTree = do
+  A.string "TREE"
+  parse32Bit
+  path <- A.takeTill (== 0)
+  A.word8 0
+  numEntries <- convertAsciiInt <$> A.takeWhile (\c -> c <= 57 && c >= 48)
+  A.word8 32
+  numSubtrees <- convertAsciiInt <$> A.takeWhile (\c -> c <= 57 && c >= 48)
+  A.word8 10
+  objName <- A.take 20
+  return $ CachedTree path numEntries numSubtrees objName
+
+extension' :: A.Parser Extension
+extension' = do
+  signature <- A.choice $ A.string <$> ["TREE", "REUC", "link", "UNTR"]
+  len <- parse32Bit
+  cont <- A.take len
+  return $ Ext signature cont
+
+extension :: A.Parser Extension
+extension = A.choice [cachedTree, extension']
+
+makeV2Flags :: Int -> Flags
+makeV2Flags i =
+  let assumeValid = i .&. 2^(15::Int) == 1
+      extended = i .&. 2^(14::Int) == 1
+      stage = i .&. (2^(13::Int) + 2^(12::Int))
+      nameLen = i `mod` 2^(12::Int)
+  in Flags assumeValid extended stage nameLen Nothing Nothing Nothing
+
+makeV3Flags :: Int -> Int -> Flags
+makeV3Flags first second =
+  let v2 = makeV2Flags first
+      reserved = second .&. 2^(15::Int) == 1
+      skipWorktree = second .&. 2^(14::Int) == 1
+      intentToAdd = second .&. 2^(13::Int) == 1
+  in v2 { reservedOf = Just reserved
+        , skipWorktreeOf = Just skipWorktree
+        , intentToAddOf = Just intentToAdd }
+
+getMode :: Int -> Mode
+getMode i =
+  let typeBits = i `div` 2 ^ (12 :: Int)
+      permBits = i `mod` 1024
+      objType = case typeBits of
+                 8 -> File
+                 10 -> Symlink
+                 14 -> Gitlink
+  in Mode objType permBits
 
 toHexes :: BS.ByteString -> String
 toHexes =
@@ -133,8 +167,15 @@ parseBytes n = do
   bytes <- A.count n A.anyWord8
   return . foldl (\acc i -> acc * 256 + fromIntegral i) 0 $ bytes
 
+convertAsciiInt :: BS.ByteString -> Int
+convertAsciiInt =
+  BS.foldl (\acc i -> fromIntegral $ i - 48 + fromIntegral acc * 10) 0
+
 parse32Bit :: A.Parser Int
 parse32Bit = parseBytes 4
 
 parse16Bit :: A.Parser Int
 parse16Bit = parseBytes 2
+
+billion :: Int
+billion = 10 ^ (9 :: Int)
